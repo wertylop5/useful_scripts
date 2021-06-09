a97d06dd180b34fc0409a6545c3a955aa2dcf05c
Stanley Lin <slin55055@gmail.com>
2021-06-09 19:53:23 -0400

    added vim update script

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # useful_scripts
 Assorted useful scripts
+
+## Descriptions
+* `update_vim.sh`: Since I build vim from source to use with YouCompleteMe, vim breaks after a system update. This will rebuild vim
+
---
+++ update_vim.sh
@@ -0,0 +1,21 @@
+if [[ $# -eq 0 ]]; then
+	echo "error: enter the directory of the vim source repo"
+	exit 1
+fi
+
+echo "starting vim update"
+
+vim_repo_path=$1
+cur_path=$(pwd)
+
+cd $vim_repo_path
+
+./configure --with-features=huge             --enable-multibyte             --enable-rubyinterp=yes             --with-python3-command=python3.9             --with-python3-config-dir=/usr/lib/python3.9/config-3.9-x86_64-linux-gnu             --enable-python3interp=yes             --enable-perlinterp=yes             --enable-luainterp=yes             --enable-gui=no             --enable-cscope
+
+make VIMRUNTIMEDIR=/usr/local/share/vim/vim82
+sudo make install
+
+cd $cur_path
+
+echo "finished update"
+
